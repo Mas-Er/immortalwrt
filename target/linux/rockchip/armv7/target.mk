8260c06b475c942f2822f36c81de53de0a120ff8
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2023-03-14 05:04:10 -0700

    Create

---
+++ target/linux/rockchip/armv7/target.mk
@@ -0,0 +1,8 @@
+ARCH:=arm
+SUBTARGET:=armv7
+BOARDNAME:=RK322x boards (32 bit)
+
+define Target/Description
+	Build firmware image for Rockchip RK322x devices.
+	This firmware features a 32 bit kernel.
+endef
