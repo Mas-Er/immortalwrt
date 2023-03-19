222a4c01a728f3e7fe068d25e57c168028084827
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2023-03-19 22:21:41 +0700

    Update atf-version.mk

--- package/boot/arm-trusted-firmware-rockchip/atf-version.mk
+++ package/boot/arm-trusted-firmware-rockchip/atf-version.mk
@@ -1,3 +1,7 @@
+RK3229_ATF:=rk322xh_bl31_v1.49.elf
+RK3229_DDR:=rk3328_ddr_333MHz_v1.19.bin
+RK3229_LOADER:=rk322xh_miniloader_v2.50.bin
+
 RK3328_ATF:=rk322xh_bl31_v1.49.elf
 RK3328_DDR:=rk3328_ddr_333MHz_v1.19.bin
 RK3328_LOADER:=rk322xh_miniloader_v2.50.bin
